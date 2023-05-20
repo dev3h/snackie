f52a50246ec5cc6176db77c1142f9e59db910988
nam <namsupermen06@gmail.com>
2023-05-20 22:11:57 +0700

    try to fix no css when deploy railway

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -29,6 +30,9 @@ class AppServiceProvider extends ServiceProvider
         // $locale = session('locale', config('app.locale'));
         // app()->setLocale($locale);
         app()->setLocale('vi');
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
 
     }
 }
